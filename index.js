4259da750c9c89d9bf0a8a4326b8fc52335205ab
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-08-16 10:45:08 +0600

    Fetch products with pagination

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 9000;
 
 const app = express();
-// 
+//
 
 const corsOptions = {
   origin: ["http://localhost:5173", "http://localhost:5174"],
@@ -15,7 +15,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_NEW_PASS}@cluster0.6u1ikeh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,25 +23,52 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    const productsCollection = client.db("vagalshop").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+// -----------
+    // Fetch products with pagination
+    app.get("/api/products", async (req, res) => {
+      const { page = 1, limit = 10 } = req.query;
+      const products = await productsCollection
+        .find()
+        .skip((page - 1) * limit)
+        .limit(parseInt(limit))
+        .toArray();
+
+      const total = await productsCollection.countDocuments();
+
+      res.json({ products, totalPages: Math.ceil(total / limit) });
+    });
+
+
+
+
+
+// ------------------------
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-run().catch(console.dir);
-
-
 
 app.get("/", (req, res) => {
   res.send("Hello from vagal-shop Server....");
